998e977f104793a99fe7c30b6137fe0e7c66ac40
Cheryl-Chun(Mac) <769303522@qq.com>
2024-08-31 08:43:27 +0800

    add symbols-outline plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,54 +1,130 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  event = { 'BufReadPre', 'BufNewFile' },
-  build = ':TSUpdate',
-  dependencies = {
-    'windwp/nvim-ts-autotag',
-    'axelvc/template-string.nvim',
-  },
-  config = function()
-    require('nvim-treesitter.configs').setup {
-      ensure_installed = {
-        'tsx',
-        'lua',
-        'vim',
-        'typescript',
-        'javascript',
-        'html',
-        'css',
-        'json',
-        'regex',
-        'rust',
-        'go',
-        'prisma',
-        'markdown',
-        'markdown_inline',
-      },
-
-      sync_install = false,
-      auto_install = true,
-      indent = {
-        enable = false,
-      },
-      highlight = {
-        enable = true,
-
-        additional_vim_regex_highlighting = false,
-      },
-      autotag = {
-        enable = true,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<enter>',
-          node_incremental = '<enter>',
-          scope_incremental = false,
-          node_decremental = '<bs>',
+  {
+    'nvim-treesitter/nvim-treesitter',
+    event = { 'BufReadPre', 'BufNewFile' },
+    build = ':TSUpdate',
+    dependencies = {
+      'windwp/nvim-ts-autotag',
+      'axelvc/template-string.nvim',
+    },
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        ensure_installed = {
+          'tsx',
+          'lua',
+          'vim',
+          'typescript',
+          'javascript',
+          'html',
+          'css',
+          'json',
+          'regex',
+          'rust',
+          'go',
+          'prisma',
+          'markdown',
+          'markdown_inline',
         },
-      },
-    }
 
-    require('template-string').setup {}
-  end,
+        sync_install = false,
+        auto_install = true,
+        indent = {
+          enable = false,
+        },
+        highlight = {
+          enable = true,
+
+          additional_vim_regex_highlighting = false,
+        },
+        autotag = {
+          enable = true,
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<enter>',
+            node_incremental = '<enter>',
+            scope_incremental = false,
+            node_decremental = '<bs>',
+          },
+        },
+      }
+
+      require('template-string').setup {}
+    end,
+  },
+  {
+    'simrat39/symbols-outline.nvim',
+    config = function()
+      require('symbols-outline').setup {
+        highlight_hovered_item = true,
+        show_guides = true,
+        auto_preview = true,
+        position = 'right',
+        relative_width = true,
+        width = 25,
+        auto_close = false,
+        show_numbers = false,
+        show_relative_numbers = false,
+        show_symbol_details = true,
+        preview_bg_highlight = 'Pmenu',
+        autofold_depth = nil,
+        auto_unfold_hover = true,
+        fold_markers = { '', '' },
+        wrap = false,
+        keymaps = { -- These keymaps can be a string or a table for multiple keys
+          close = { '<Esc>', 'q' },
+          goto_location = '<Cr>',
+          focus_location = 'o',
+          hover_symbol = '<C-space>',
+          toggle_preview = 'K',
+          rename_symbol = 'r',
+          code_actions = 'a',
+          fold = 'h',
+          unfold = 'l',
+          fold_all = 'W',
+          unfold_all = 'E',
+          fold_reset = 'R',
+        },
+        lsp_blacklist = {},
+        symbol_blacklist = {},
+        symbols = {
+          File = { icon = '', hl = '@text.uri' },
+          Module = { icon = '', hl = '@namespace' },
+          Namespace = { icon = '', hl = '@namespace' },
+          Package = { icon = '', hl = '@namespace' },
+          Class = { icon = '𝓒', hl = '@type' },
+          Method = { icon = 'ƒ', hl = '@method' },
+          Property = { icon = '', hl = '@method' },
+          Field = { icon = '', hl = '@field' },
+          Constructor = { icon = '', hl = '@constructor' },
+          Enum = { icon = 'ℰ', hl = '@type' },
+          Interface = { icon = 'ﰮ', hl = '@type' },
+          Function = { icon = '', hl = '@function' },
+          Variable = { icon = '', hl = '@constant' },
+          Constant = { icon = '', hl = '@constant' },
+          String = { icon = '𝓐', hl = '@string' },
+          Number = { icon = '#', hl = '@number' },
+          Boolean = { icon = '⊨', hl = '@boolean' },
+          Array = { icon = '', hl = '@constant' },
+          Object = { icon = '⦿', hl = '@type' },
+          Key = { icon = '🔐', hl = '@type' },
+          Null = { icon = 'NULL', hl = '@type' },
+          EnumMember = { icon = '', hl = '@field' },
+          Struct = { icon = '𝓢', hl = '@type' },
+          Event = { icon = '🗲', hl = '@type' },
+          Operator = { icon = '+', hl = '@operator' },
+          TypeParameter = { icon = '𝙏', hl = '@parameter' },
+          Component = { icon = '', hl = '@function' },
+          Fragment = { icon = '', hl = '@constant' },
+        },
+      }
+      vim.api.nvim_set_keymap(
+        'n',
+        '<leader>o',
+        ':SymbolsOutline<CR>',
+        { noremap = true, silent = true }
+      )
+    end,
+  },
 }
